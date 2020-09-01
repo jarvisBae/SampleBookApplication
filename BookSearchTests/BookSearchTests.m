2973e929b2be0f07c0ac524aab327f4ef2dc4408
JINKI BAE <jingi@risefactory.co.kr>
2020-09-01 23:20:19 +0900

    unit test 테스트 케이스 추가 및 주석 추가

--- BookSearchTests/BookSearchTests.m
+++ BookSearchTests/BookSearchTests.m
@@ -12,6 +12,9 @@
 #import "BookDetailViewModel.h"
 #import "SearchTextViewModel.h"
 #import "Functions.h"
+#import "NSCache+Instance.h"
+#import "BookCache.h"
+#import "BookDisplay.h"
 
 @interface BookSearchTests : XCTestCase
 
@@ -34,19 +37,23 @@
     }];
 }
 
+/**
+ * books api 테스트 : 각각 동일한 검색어를 입력한 경우에 결과가 동일여부 테스트
+ */
 - (void)testGetBooks {
     BookViewModel *bookViewModel = [[BookViewModel alloc] init];
     
+    NSString *query = @"book";
     __block NSArray<BookDisplay *> *resultBooksFirst = nil;
     __block NSArray<BookDisplay *> *resultBooksSecond = nil;
     
-    [bookViewModel getBooksWithQuery:@"book" page:@"1" success:^(NSArray<BookDisplay *> * _Nonnull books, NSInteger total, NSInteger page) {
+    [bookViewModel getBooksWithQuery:query page:@"1" success:^(NSArray<BookDisplay *> * _Nonnull books, NSInteger total, NSInteger page) {
         resultBooksFirst = books;
     } error:^(NSError * _Nonnull error) {
 
     }];
     
-    [bookViewModel getBooksWithQuery:@"book" page:@"2" success:^(NSArray<BookDisplay *> * _Nonnull books, NSInteger total, NSInteger page) {
+    [bookViewModel getBooksWithQuery:query page:@"1" success:^(NSArray<BookDisplay *> * _Nonnull books, NSInteger total, NSInteger page) {
         resultBooksSecond = books;
     } error:^(NSError * _Nonnull error) {
 
@@ -56,10 +63,20 @@
     XCTWaiter *waiter = [[XCTWaiter alloc] init];
     [waiter waitForExpectations:@[expectation] timeout:5.0];
     
-    XCTAssertEqualObjects(resultBooksFirst, resultBooksFirst);
-    XCTAssertEqualObjects(resultBooksSecond, resultBooksSecond);
+    XCTAssertEqual(resultBooksFirst.count, resultBooksSecond.count);
+    
+    for (int i=0; i<resultBooksFirst.count; i++) {
+        BookDisplay *bookDisplayFirst = [resultBooksFirst objectAtIndex:i];
+        BookDisplay *bookDisplaySecond = [resultBooksSecond objectAtIndex:i];
+        
+        XCTAssertEqualObjects(bookDisplayFirst.title, bookDisplaySecond.title);
+        XCTAssertEqualObjects(bookDisplayFirst.subtitle, bookDisplaySecond.subtitle);
+        XCTAssertEqualObjects(bookDisplayFirst.isbn13, bookDisplaySecond.isbn13);
+    }
 }
-
+/**
+ * book detail api 테스트 : 각각 동일한 검색어를 입력한 경우에 결과가 동일여부 테스트
+*/
 - (void)testGetBookDetail {
     NSString *isbn13 = @"9780596806606";
     
@@ -85,10 +102,13 @@
     XCTWaiter *waiter = [[XCTWaiter alloc] init];
     [waiter waitForExpectations:@[expectation] timeout:5.0];
 
-    XCTAssertEqualObjects(bookDetailDisplayFirst, bookDetailDisplayFirst);
-    XCTAssertEqualObjects(bookDetailDisplaySecond.isbn13.description, isbn13.description);
+    XCTAssertEqualObjects(bookDetailDisplayFirst.isbn13, isbn13);
+    XCTAssertEqualObjects(bookDetailDisplaySecond.isbn13, isbn13);
 }
 
+/**
+ * 사용자가 검색어를 입력한 경우에 저장 여부 테스트
+*/
 - (void)testLocalCacheSaveAndLoad {
     
     NSString *inputText = @"Hello!!!";
@@ -107,7 +127,9 @@
     
     XCTAssertEqualObjects(inputText, [arr firstObject].text);
 }
-
+/**
+ * 사용자가 검색어를 입력한 경우에 저장 여부 테스트
+*/
 - (void)testFunctions {
     
     NSString *saveData = @"hello";
@@ -118,6 +140,47 @@
     NSString *savedData = [Functions loadDataFromPlistForKey:saveDataKey];
     
     XCTAssertEqualObjects(saveData, savedData);
+}
+
+/**
+ *  books api호출 후 결과 정보가 nscache 저장 여부 테스트
+*/
+- (void)testGetBooksAndCacheCompare {
+    
+    NSString *query = @"book";
+    BookViewModel *bookViewModel = [[BookViewModel alloc] init];
+    __block NSArray<BookDisplay *> *resultBooksFirst = nil;
+    __block NSMutableArray * items = [[NSMutableArray alloc] init];
+    
+    [bookViewModel getBooksWithQuery:query page:@"1" success:^(NSArray<BookDisplay *> * _Nonnull books, NSInteger total, NSInteger page) {
+        resultBooksFirst = books;
+        
+        BookCache *bookCache = [NSCache.manager objectForKey:query];
+        for (Book *book in bookCache.books) {
+            [items addObject:[[BookDisplay alloc] initWithBook:book]];
+        }
+    } error:^(NSError * _Nonnull error) {
+
+    }];
+    
+    XCTestExpectation *expectation = [[XCTestExpectation alloc] initWithDescription:@"APIPrivoderTaskExpectation"];
+    XCTWaiter *waiter = [[XCTWaiter alloc] init];
+    [waiter waitForExpectations:@[expectation] timeout:5.0];
+    
+    XCTAssertEqual(resultBooksFirst.count, items.count);
+
+    for (int i=0; i<items.count; i++) {
+        BookDisplay *bookDisplayForServer = [resultBooksFirst objectAtIndex:i];
+        BookDisplay *bookDisplayForCache = [items objectAtIndex:i];
+        
+        XCTAssertEqual(bookDisplayForServer.title, bookDisplayForCache.title);
+        XCTAssertEqual(bookDisplayForServer.subtitle, bookDisplayForCache.subtitle);
+        XCTAssertEqual(bookDisplayForServer.isbn13, bookDisplayForCache.isbn13);
+    }
+    
+    
+    
+    
 }
 
 @end
